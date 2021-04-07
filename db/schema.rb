116f6acf5b9e6f30ff8707055889d5031dbf3887
sanikin1 <sanikin1@cs6242-vm05.uwg.westga.edu>
2021-04-07 13:59:03 -0400

    Created User table and configured it with Devise modules.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_022015) do
+ActiveRecord::Schema.define(version: 2021_04_07_174120) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,8 +20,20 @@ ActiveRecord::Schema.define(version: 2021_03_18_022015) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "split", default:0
-    t.integer "nosplit", default:0
+    t.integer "split"
+    t.integer "nosplit"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
