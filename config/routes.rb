ab29d3e374a83655c41d4d583f554acee2f6b584
sanikin1 <sanikin1@cs6242-vm01.uwg.westga.edu>
2021-04-08 13:46:09 -0400

    Fixed redirection links.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'restaurants#index'
   post "restaurants/:id/split" => "restaurants#split", as: "restaurants_split"
   post "restaurants/:id/nosplit" => "restaurants#nosplit", as: "restaurants_nosplit"
 
