d6be11adda91c9508c745270dc6c22051d6eb275
sanikin1 <sanikin1@cs6242-vm05.uwg.westga.edu>
2021-04-25 14:28:13 -0400

    Generated Comment's scaffold and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_011943) do
+ActiveRecord::Schema.define(version: 2021_04_25_182102) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
