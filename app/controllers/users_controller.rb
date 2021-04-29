ee057d9454cf26f3f2319a97fb735e3f518c2bbe
sanikin1 <sanikin1@cs6242-vm02.uwg.westga.edu>
2021-04-29 17:34:03 -0400

    Generated Favorites scaffold and migration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,10 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:show]
   before_action :authenticate_user!, only: [:show]
-
   def show
     @user = current_user
     @comments = @user.comments
     @votes = @user.votes.group(:restaurant_id)
-
   end
 
   private
--- app/models/vote_history.rb
+++ app/models/vote_history.rb
@@ -1,4 +1,9 @@
 class VoteHistory < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+def get_restaurant
+    @id = self.restaurant_id
+    @restaurants = Restaurant.where("id = ?", "#{@id}").first
+  end
 end
